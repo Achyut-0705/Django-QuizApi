a36d89082da4ee4f6e07f9edc925fd164efcdf1d
Achyut <achyut.shukla.btech2020@sitpune.edu.in>
2021-10-31 18:06:47 +0530

    update

--- WebApis/settings.py
+++ WebApis/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-!nvrr)s2r3bg8c8xz@63r@9)=p+(3c@ma9ct4qo5b!dti%j3tr'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-!nvrr)s2r3bg8c8xz@63r@9)=p+(3c@ma9ct4qo5b!dti%j3tr'
+# SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
