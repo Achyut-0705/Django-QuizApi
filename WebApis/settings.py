3bd7e2c47662b561a23088784b8634b7b64ebc97
Achyut <achyut.shukla.btech2020@sitpune.edu.in>
2021-10-31 18:23:44 +0530

    push

--- WebApis/settings.py
+++ WebApis/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://ncertquizapi.herokuapp.com']
+# ALLOWED_HOSTS = ['https://ncertquizapi.herokuapp.com']
+ALLOWED_HOSTS = ['ncertquizapi.herokuapp.com']
 
 
 # Application definition
